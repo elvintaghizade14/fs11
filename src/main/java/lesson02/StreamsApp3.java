4f04460754c01031e16ff1d2438daf1ef2abd332
Alexey <alexey.rykhalskiy@gmail.com>
2020-08-29 10:20:45 +0300

    -- lesson2

--- src/main/java/lesson02/StreamsApp3.java
+++ src/main/java/lesson02/StreamsApp3.java
@@ -1,7 +1,10 @@
 package lesson02;
 
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamsApp3 {
@@ -12,12 +15,20 @@ public class StreamsApp3 {
    */
 
   public static void main(String[] args) {
-    int[][] x= {
+    int[][] x = {
         {1,2,3,4},
         {5,6},
         {},
         {7,8,9,10}
     };
+    Stream<int[]> s1 = Arrays.stream(x);
+    Stream<Integer> s2a = s1.flatMap(a -> Arrays.stream(a).boxed());
+
+    IntStream s2 = s1.flatMapToInt(a -> Arrays.stream(a));
+    int[] flattened = s2.toArray();
+    Stream<Integer> s3 = s2.boxed();
+    List<Integer> collected = s3.collect(Collectors.toList());
+    collected.forEach(System.out::println);
 
   }
 
